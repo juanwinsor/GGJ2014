286b62c8470a7636aa45d389359b76f98d638632
juan winsor <juan.winsor@gmail.com>
2014-01-25 16:15:41 -0500

    made physics layers and sorting layers
    added edge collision to bounds of test map for testing

---
+++ Assets/Scripts/Game/PhysicsManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Physics2D.IgnoreLayerCollision (9, 9);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
