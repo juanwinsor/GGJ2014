cc54a60fad5b81fb396d974781d289534b6742ad
Phil <pmasson993@gmail.com>
2014-01-25 13:02:46 -0500

    Added Movement and AIController Script

---
+++ Assets/Scripts/Movement/Movement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MoveDir(int dir)
+	{
+		Vector2 direction = new Vector2(0,0);
+		switch(dir)
+		{
+		case 1:
+			direction = new Vector2(0,1);
+			break;
+		case 2:
+			direction = new Vector2(0,-1);
+			break;
+		case 3:
+			direction = new Vector2(-1,0);
+			break;
+		case 4:
+			direction = new Vector2(1,0);
+			break;
+		default:
+			direction = Vector2.zero;
+			break;
+			
+		}
+		
+		this.gameObject.rigidbody2D.AddForce(direction * 30.0f);
+	}
+}
---
+++ Assets/Scripts/NPC/AIController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : MonoBehaviour {
+
+	int theDir = 0;
+	float moveTimer = 0;
+	float timeToNextMove = 0;
+
+	public Movement movementScript;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		calcTime();
+		
+		if(timeToNextMove <= moveTimer)
+		{
+			timeToNextMove = Random.value * 3.0f;
+			theDir = Random.Range(0,4);
+			moveTimer = 0;
+
+			movementScript.MoveDir (theDir);
+		}		
+
+	}
+
+	void calcTime()
+	{
+		moveTimer += Time.deltaTime;
+	}
+}
