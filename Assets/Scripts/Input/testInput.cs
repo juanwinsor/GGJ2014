849fe64f3d27d5b2024a6307631d85e45d991c6f
juan winsor <juan.winsor@gmail.com>
2014-01-25 15:27:28 -0500

    added sprites, damagers

---
+++ Assets/Scripts/Input/testInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testInput : MonoBehaviour {
+
+	public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			animator.SetTrigger("attack");
+		}
+	}
+}
---
+++ Assets/Scripts/Player/AttackEvents.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackEvents : MonoBehaviour {
+
+	public GameObject damageDealer;
+	public float activationTime = 0.1f;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void startAttack()
+	{
+		Debug.Log ("attack");
+		damageDealer.SetActive (true);
+	}
+
+	void endAttack()
+	{
+		Debug.Log ("stopattack");
+		damageDealer.SetActive (false);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		
+	}
+	void OnTriggerExit2D(Collider2D other) 
+	{
+		
+	}
+	void OnTriggerStay2D(Collider2D other) 
+	{
+		
+	}
+
+}
