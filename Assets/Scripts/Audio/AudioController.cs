76ab6df93c823e242e35c789757e03365bcf22f1
Juan Winsor <juan.winsor@gmail.com>
2014-01-26 01:32:20 -0500

    added audio

---
+++ Assets/Scripts/Audio/AudioController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour {
+
+	public AudioClip musicAmbient;
+	public AudioClip musicTerror;
+	public AudioClip musicRampage;
+	public AudioClip sfxGun;
+	public AudioClip sfxScratch;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Game/GameSessionManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSessionManager : MonoBehaviour {
+
+	public AudioController audioController;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.A))
+		{
+			//this.audio.clip = audioController.musicAmbient;
+			//this.audio.loop = true;
+			//this.audio.Play();
+		}
+	}
+
+	//when a game starts
+	void GameStart()
+	{
+
+	}
+
+	//when rampage mode starts
+	void RampageModeStart()
+	{
+
+	}
+
+	void BeastAttack()
+	{
+
+	}
+
+	void HumanAttack()
+	{
+
+	}
+
+}
