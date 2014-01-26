36285d20dc15a0dc0d099435fa037109d991b34c
PhilM1 <pmasson993@gmail.com>
2014-01-26 14:11:57 -0500

    Added Beast Attack range on turbo mode. Added Tint for turbo mode.

---
+++ Assets/Scripts/Player/BeastGrowth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeastGrowth : MonoBehaviour {
+
+	Timer turboTimer = new Timer(90);
+
+	// Use this for initialization
+	void Start () {
+		turboTimer.Start ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (turboTimer.Done) {
+			this.gameObject.transform.localScale = new Vector3(1.5f,1.5f,1);
+				}
+	
+	}
+}
---
+++ Assets/Scripts/Player/BeastTint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeastTint : MonoBehaviour {
+
+	Timer turbo = new Timer(90);
+
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (turbo.Done) {
+			GetComponent<SpriteRenderer>().color = Color.red;
+				}
+	}
+}
