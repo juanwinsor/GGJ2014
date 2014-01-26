58a45e39cd43924184b14667fa7d31b8e778f9b4
PhilM1 <pmasson993@gmail.com>
2014-01-26 14:27:38 -0500

    Added Smile after player death

--- Assets/Scripts/Effects/DeathMarker.cs
+++ Assets/Scripts/Effects/DeathMarker.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class DeathMarker : MonoBehaviour {
 
+	float thesize = 0.1f;
+	float speed = 1.0f;
 	// Use this for initialization
 	void Start () {
-		Destroy(gameObject, 0.7f);
+		Destroy(gameObject, 1.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		thesize += speed * Time.deltaTime;
+		this.gameObject.transform.localScale = new Vector3 (thesize, thesize, 1.0f);
 	
 	}
 }
--- Assets/Scripts/Player/Die.cs
+++ Assets/Scripts/Player/Die.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Die : MonoBehaviour {
 
 	public GameSessionController gameSession;
+	public GameObject smilePrefab;
 
 	// Use this for initialization
 	void Start () {
@@ -22,6 +23,7 @@ public class Die : MonoBehaviour {
 			gameSession.SpawnBlood (this.transform.position);
 			if(this.tag == "Human")
 			{
+				GameObject smile = Instantiate(smilePrefab) as GameObject;
 				gameSession.NumberOfPlayers--;
 			}
 			if(this.tag == "Beast")
